4540115577f4fcbc51eb0375fdabe6932a851fac
JoshuaConcon <conconjoshua@gmail.com>
2019-07-17 22:18:40 -0400

    (#2) Changed container tag name

--- utscfood-api/app.py
+++ utscfood-api/app.py
@@ -1,12 +1,24 @@
 from flask import Flask
+from redis import Redis, RedisError
+import os
+import socket
 
-# flask object
+# Connect to Redis
+redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
+
 app = Flask(__name__)
 
-@app.route('/')
-def hellow_world():
-  return 'Hello, World!'
+@app.route("/")
+def hello():
+    try:
+        visits = redis.incr("counter")
+    except RedisError:
+        visits = "<i>cannot connect to Redis, counter disabled</i>"
+
+    html = "<h3>Hello {name}!</h3>" \
+           "<b>Hostname:</b> {hostname}<br/>" \
+           "<b>Visits:</b> {visits}"
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
 
 if __name__ == "__main__":
-  # run app if file is run directly
-  app.run()
+    app.run(host='0.0.0.0', port=80)
